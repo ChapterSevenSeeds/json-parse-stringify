facfa46a6b52d063eb097a28bcc931dea7f64077
Tyson Jones <ballisticmissle@gmail.com>
2022-02-06 15:17:45 -0700

    Implemented performance

--- tester.js
+++ tester.js
@@ -1,3 +1,38 @@
 const parse = require("./parse");
 
-console.log(parse("True"))
+const tests = {
+    string: JSON.stringify("hello"),
+    number: JSON.stringify(235.1),
+    boolean: JSON.stringify(false),
+    array: JSON.stringify([3, 2, 5, null, false, 1, 235235]),
+    object: JSON.stringify({ "d": 3, "a": "asdf", "q": false }),
+    nestedArray: JSON.stringify([3, [3, 2, 5, [1, [1, [5]]]], 5, ["asdf", false, null]]),
+    nestedObject: JSON.stringify({ "d": { "a": "asdf", "b": { "3": false } } }),
+};
+
+const types = [
+    {
+        name: "JSON.parse",
+        function: JSON.parse
+    },
+    {
+        name: "Tyson's parse",
+        function: parse
+    }
+];
+
+let start, stop;
+const COUNT = 1000000;
+console.log(`Running ${COUNT} tests...\n`);
+for (const key of Object.keys(tests)) {
+    console.log(key);
+    for (const type of types) {
+        start = performance.now();
+        for (let i = 0; i < COUNT; ++i) {
+            type.function(tests[key]);
+        }
+        stop = performance.now();
+        console.log(`${type.name}: ${stop - start} milliseconds`);
+    }
+    console.log();
+}
