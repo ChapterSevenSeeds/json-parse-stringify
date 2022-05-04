5faae92b47ad245f9bbce23ad197a5eab2838b45
Tyson Jones <ballisticmissle@gmail.com>
2022-05-03 21:32:20 -0600

    Follow sets are weird

--- grammar.js
+++ grammar.js
@@ -34,6 +34,42 @@ class Rule {
     }
 }
 
+function deepIsEqual(obj1, obj2) {
+    const typeofObj1 = typeof (obj1);
+    const typeofObj2 = typeof (obj2);
+
+    if (typeofObj1 !== typeofObj2) return false;
+
+    switch (typeofObj1) {
+        case "number":
+        case "string":
+        case "boolean":
+            return obj1 === obj2;
+        case "object":
+            if (obj1 instanceof Set !== obj2 instanceof Set) return false;
+            if ((obj1 == null) !== (obj2 == null)) return false;
+            if (Array.isArray(obj1) !== Array.isArray(obj2)) return false;
+
+            if (obj1 instanceof Set) {
+                return deepIsEqual(Array.from(obj1), Array.from(obj2));
+            }
+
+            if (Array.isArray(obj1)) {
+                if (obj1.length !== obj2.length) return false;
+                for (let i = 0; i < obj1.length; ++i) {
+                    if (!deepIsEqual(obj1[i], obj2[i])) return false;
+                }
+            }
+
+            for (const key in obj1) {
+                if (!(key in obj2)) return false;
+                if (!deepIsEqual(obj1[key], obj2[key])) return false;
+            }
+
+            return true;
+    }
+}
+
 function singletonChar(hexOrChar) {
     if (hexOrChar.length > 1) {
         return String.fromCodePoint(parseInt(hexOrChar, 16));
@@ -42,12 +78,16 @@ function singletonChar(hexOrChar) {
     return hexOrChar;
 }
 
-const grammar = require('fs').readFileSync("JSON grammar.txt").toString();
+const grammar = require('fs').readFileSync("test grammar 3.txt").toString();
 
 const ruleGroups = {};
-
+let startVariable;
 for (const group of grammar.matchAll(/(.+?)(?:(?:\r\n)|\n)((?:[ \t]+.*?(?:(?:\r\n)|\n|$))+)/g)) {
     const rule = new Rule(false, group[1].trim());
+    if (!startVariable) {
+        startVariable = rule.Identifier;
+    }
+
     for (const line of group[2].split(/(\r\n)|\n/).map(x => x.trim()).filter(x => x)) {
         rule.Replacements.push([]);
         for (let i = 0; i < line.length; ++i) {
@@ -83,7 +123,7 @@ for (const group of grammar.matchAll(/(.+?)(?:(?:\r\n)|\n)((?:[ \t]+.*?(?:(?:\r\
                     while (line[++i] !== "'") endRange += line[i]; // Grab the ending of the range.
                     endRange = singletonChar(endRange);
                     while (line[++i] === " "); // Consume whitespace.
-                    
+
                     const exclusions = [];
                     while (line[i] === '-') {
                         let exclusionChar = "";
@@ -167,5 +207,32 @@ function first(variable) {
     return result;
 }
 
-const firstSets = Object.values(ruleGroups).filter(x => !x.Terminal).reduce((acc, cur) => Object.assign(acc, { [cur.Identifier]: first(cur.Identifier) }), {});
+function follow(followSets, startVariable) {
+    followSets[startVariable].add("$");
+
+    let copy = {};
+
+    do {
+        copy = Object.keys(followSets).reduce((acc, cur) => Object.assign(acc, { [cur]: new Set(Array.from(followSets[cur])) }), {});
+
+        for (const variable in followSets) {
+            for (const replacement of ruleGroups[variable].Replacements) {
+                if (replacement.length === 3 && !firstSets[replacement[2].Identifier].has("")) {
+                    const firstBeta = new Set(firstSets[replacement[2].Identifier]);
+
+                    followSets[replacement[1].Identifier].extend(firstBeta);
+                } else if (replacement.length === 3 && firstSets[replacement[2].Identifier].has("")) {
+                    followSets[replacement[1].Identifier].extend(followSets[variable]);
+                } else if (replacement.length === 2) {
+                    followSets[replacement[1].Identifier].extend(followSets[variable]);
+                }
+            }
+        }
+
+    } while (!deepIsEqual(followSets, copy));
+}
+
+const firstSets = Object.values(ruleGroups).reduce((acc, cur) => Object.assign(acc, { [cur.Identifier]: first(cur.Identifier) }), {});
+const followSets = Object.values(ruleGroups).reduce((acc, cur) => Object.assign(acc, { [cur.Identifier]: new Set() }), {});
+follow(followSets, startVariable);
 console.log(firstSets);
